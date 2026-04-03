0b8d43ece7a0639f437484d213597ef45e678b3d
InHo <qheogus55@naver.com>
2026-04-04 05:59:17 +0900

    GA 태그 등록

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,21 @@ import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import TestPage from './pages/TestPage';
 import ResultPage from './pages/ResultPage';
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize([
+      {
+        trackingId: import.meta.env.VITE_GA_TRACKING_ID,
+        gaOptions: {
+          siteSpeedSampleRate: 100,
+        },
+      },
+    ]);
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="min-h-screen w-full" style={{ backgroundColor: '#f5f5f5' }}>
