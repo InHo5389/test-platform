ee82a674fe79ca22db5cbf4dda681e1d6f6ba5b1
InHo <qheogus55@naver.com>
2026-04-05 14:38:24 +0900

    defineConfig수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import prerender from "@prerenderer/rollup-plugin";
 import PuppeteerRenderer from "@prerenderer/renderer-puppeteer";
+import puppeteer from "puppeteer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,31 +11,21 @@ export default defineConfig({
     react(),
     tailwindcss(),
     prerender({
-      routes: [
-        "/",
-        "/test/personalColor",
-        "/privacy",
-      ],
+      routes: ["/", "/test/personalColor", "/privacy"],
       renderer: PuppeteerRenderer,
-      server: {
-        port: 3000,
-        host: "localhost",
-      },
+      server: { port: 3000, host: "localhost" },
       rendererOptions: {
         maxConcurrentRoutes: 1,
         renderAfterTime: 2000,
         launchOptions: {
-          executablePath: "C:\\Users\\qheog\\.cache\\puppeteer\\chrome\\win64-146.0.7680.153\\chrome-win64\\chrome.exe",
+          executablePath: puppeteer.executablePath(),
           args: ["--no-sandbox", "--disable-setuid-sandbox"],
         },
       },
       postProcess(renderedRoute) {
         renderedRoute.html = renderedRoute.html
           .replace(/http:/i, "https:")
-          .replace(
-            /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
-            "arabogae.shop"
-          );
+          .replace(/(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i, "arabogae.shop");
       },
     }),
   ],
